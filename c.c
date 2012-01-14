d2d94b0124eab6551373db87037efa406b187448
akaedu <akaedu@xt.(none)>
2012-01-14 09:14:23 +0800

    bbb

---
+++ c.c
@@ -0,0 +1,17 @@
+/************************************************************************
+ *       Filename:  c.c
+ *    Description:  
+ *        Version:  1.0
+ *        Created:  01/14/2012 09:13:44 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *         Author:  xutao (But_Bueatiful) 775932080@qq.com
+ *   Organization:  akaedu.org
+ ************************************************************************/
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("ccccccccccccccccc\n");
+	return 0;
+}
