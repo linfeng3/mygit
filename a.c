ddcdcba2a18a1edb207a472fd9abf31b3d7f86b3
akaedu <akaedu@xt.(none)>
2012-01-14 09:09:06 +0800

    aaa

---
+++ a.c
@@ -0,0 +1,17 @@
+/************************************************************************
+ *       Filename:  a.c
+ *    Description:  
+ *        Version:  1.0
+ *        Created:  01/14/2012 09:07:42 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *         Author:  xutao (But_Bueatiful) 775932080@qq.com
+ *   Organization:  akaedu.org
+ ************************************************************************/
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("aaaaaaaaaaaaaaaa\n");
+	return 0;
+}
---
+++ b.c
@@ -0,0 +1,17 @@
+/************************************************************************
+ *       Filename:  b.c
+ *    Description:  
+ *        Version:  1.0
+ *        Created:  01/14/2012 09:08:16 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *         Author:  xutao (But_Bueatiful) 775932080@qq.com
+ *   Organization:  akaedu.org
+ ************************************************************************/
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("bbbbbbbbbbbbbbbbbbbb\n");
+	return 0;
+}
